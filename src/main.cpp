63993d42be74a92457fab052e449c5575a623d92
JoaoGaValentim <joao.gabriel.vale.theodoro@gmail.com>
2025-08-14 21:53:24 -0300

    feat: compress functions and methods

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
  * @date 2025-08-14
  * */
 #include "../include/Document.hpp"
+#include <cctype>
 #include <cstdlib>
 #include <format>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -18,42 +20,47 @@ void help() {
   std::cout << "  -cpf <opt-total> to generate CPF" << '\n';
   std::cout << "  -cnpj <opt-total> to generate CNPJ" << '\n';
   std::cout << "  -h or -help <opt-total> to open this documentation" << '\n';
+  std::cout
+      << "  -o [file-name] use with \"docGen [doc] [total >= 1] -o [file]\""
+      << "\n";
   std::cout << "  -v or -version show version." << '\n';
 }
 
 void version() { std::cout << VERSION << '\n'; }
 
-void generate_document(std::vector<std::string> &directives, std::string &arg) {
+std::string generate_document(std::vector<std::string> &directives,
+                              std::string &arg) {
   bool is_in_directive =
       std::find(directives.begin(), directives.end(), arg) != directives.end();
-  // Verifica se o argumento está na lista de diretivas
-  if (is_in_directive) {
-    std::string docType = arg.substr(1); // Remove o '-'
-    Document document(docType);
-    std::cout << std::format("DOCUMENT = {0}", document.generate()) << "\n";
-  }
-
-  if (!is_in_directive) {
-    help();
-    return;
-  }
+  std::string docType = arg.substr(1); // Remove o '-'
+  Document document(docType);
+  return !is_in_directive ? "" : document.generate();
 }
 
-void generate_document_it(int &total, std::vector<std::string> &directives,
-                          std::string arg) {
+std::vector<std::string>
+generate_document_it(int &total, std::vector<std::string> &directives,
+                     std::string arg) {
+std:;
+  std::vector<std::string> documents{};
   for (size_t i = 0; i < total; i++) {
-    generate_document(directives, arg);
+    documents.push_back(generate_document(directives, arg));
   }
-  return;
+  return documents;
 }
 
-void execute_commands(std::vector<std::string> directives, int &argc,
+void execute_commands(std::vector<std::string> &directives, int &argc,
                       const char **argv) {
   std::string arg = argv[1];
-  int total_args = 1;
+  std::string document{};
+  std::vector<std::string> documents{};
+  bool is_in_directive =
+      std::find(directives.begin(), directives.end(), arg) != directives.end();
 
-  if (arg == "-version" || arg == "-v") {
-    version();
+  bool show_help = arg == "-version" || arg == "-v" || !arg.starts_with('-') ||
+                   !is_in_directive;
+
+  if (show_help) {
+    help();
     return;
   }
 
@@ -62,13 +69,21 @@ void execute_commands(std::vector<std::string> directives, int &argc,
     return;
   }
 
-  if (argc == 2) {
-    generate_document_it(total_args, directives, arg);
+  if (argc == 2 || argc == 3) {
+    int total_args = !argv[2] ? 1 : std::isdigit(*argv[2]) ? atoi(argv[2]) : 0;
+    if (total_args == 0) {
+      help();
+      return;
+    }
+    documents = generate_document_it(total_args, directives, arg);
+    for (auto &document_value : documents) {
+      std::cout << std::format("DOCUMENTO = {0}", document_value) << '\n';
+    }
   }
 
-  if (argc == 3) {
-    total_args = atoi(argv[2]);
-    generate_document_it(total_args, directives, arg);
+  if (argc > 3) {
+    help();
+    return;
   }
 }
 
